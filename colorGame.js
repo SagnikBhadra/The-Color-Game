f58e8ffd2d2264e85e5dd02063508c5b11de55cc
SagnikBhadra <bhad1434@pacificu.edu>
2018-12-16 22:06:13 -0800

    Added header with span

--- colorGame.js
+++ colorGame.js
@@ -11,7 +11,7 @@ var squares = document.querySelectorAll(".square");
 var pickedColor = colors[3];
 var colorDisplay = document.getElementById("colorDisplay");
 
-//colorDisplay.textContent = pickedColor;
+colorDisplay.textContent = pickedColor;
 
 for (var i = 0; i < squares.length; i++){
 	squares[i].style.backgroundColor = colors[i];
